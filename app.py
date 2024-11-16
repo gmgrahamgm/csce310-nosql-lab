a77359aab9d719dbf0567491e3f1562e100dbd78
gmgrahamgm <grahamd@tamu.edu>
2024-11-16 00:36:29 -0600

    Updated app.py to run with new tkinter class.

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-import os
-from dotenv import load_dotenv
-from pymongo import MongoClient
+import tinker_gui as tkgui
 
-load_dotenv()
-db_password = os.getenv('DB_PASSWORD')
-if not db_password:
-    raise ValueError("DB_PASSWORD is not set in the environment variables.")
-
-connection_string = f"mongodb+srv://grahamd:{
-    db_password}@cluster0.dxb95.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(connection_string)
+if __name__ == "__main__":
+    app = tkgui.TkinterApp()
+    app.run()
